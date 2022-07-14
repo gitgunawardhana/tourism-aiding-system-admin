344c25a733314e8124d6af9df1cee29434b08768
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-15 01:48:19 +0530

    Updated configurations js file

--- src/Pages/Configurations/Configurations.js
+++ src/Pages/Configurations/Configurations.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 
-function Configurations(){
+function Configurations() {
+
+    useEffect(() => {
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    }, []);
+
     return (
         <div className="configurations">
             <h1>Configurations</h1>
