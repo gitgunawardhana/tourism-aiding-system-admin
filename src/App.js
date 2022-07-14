582166e8a67cb3341825af195a6036fbd0883413
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-15 01:49:14 +0530

    Updated main files

--- src/App.js
+++ src/App.js
@@ -8,24 +8,27 @@ import Users from "./Pages/Users/Users";
 import Vendors from "./Pages/Vendors/Vendors";
 import SingleVendor from "./Pages/SingleVendor/SingleVendor";
 import SingleAccommodation from "./Pages/SingleAccommodation/SingleAccommodation";
+import NewAccommodation from "./Pages/NewAccommodation/NewAccommodation";
 
 function App() {
-  return (
-      <>
-        <Router>
-          <NavigationPanel/>
-          <Routes>
-            <Route path='/' exact element={<Home/>}/>
-            <Route path='/configurations' element={<Configurations/>}/>
-            <Route path='/users' element={<Users/>}/>
-            <Route path='/vendors' element={<Vendors/>}/>
-            <Route path='/vendor' element={<SingleVendor/>}/>
-            <Route path='/vendor' element={<SingleVendor/>}/>
-            <Route path='/accommodation' element={<SingleAccommodation/>}/>
-          </Routes>
-        </Router>
-      </>
-  );
+    return (
+        <>
+            <Router>
+                <NavigationPanel/>
+                <Routes>
+                    <Route path='/' exact element={<Home/>}/>
+                    <Route path='/configurations' element={<Configurations/>}/>
+                    <Route path='/users' element={<Users/>}/>
+                    <Route path='/vendors' element={<Vendors/>}/>
+                    <Route path='/vendor' element={<SingleVendor/>}/>
+                    <Route path='/vendor' element={<SingleVendor/>}/>
+                    <Route path='/accommodation' element={<SingleAccommodation/>}/>
+                    <Route path='/accommodation/:action' element={<SingleAccommodation/>}/>
+                    <Route path='/accommodation/new' element={<NewAccommodation/>}/>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
