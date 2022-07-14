9edc6419b5e4fc9c9558741f94f534f11c517ced
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-15 01:48:36 +0530

    Updated navigation panel

--- src/Components/NavigationPanel/NaigationPanelData.js
+++ src/Components/NavigationPanel/NaigationPanelData.js
@@ -1,14 +1,15 @@
 import React from "react";
-import Home from "../../Assets/NavigationPanel/home.png";
+import Dashboard from "../../Assets/NavigationPanel/dashboard.png";
 import Vendors from "../../Assets/NavigationPanel/vendors.png";
 import Users from "../../Assets/NavigationPanel/users.png";
 import Configurations from "../../Assets/NavigationPanel/configurations.png";
+import Locations from "../../Assets/NavigationPanel/locations.png";
 
 export const NavigationPanelData = [
     {
-        title: 'Home',
+        title: 'Dashboard',
         path: '/',
-        icon: <img src={Home} alt="Home Icon" className="nav-bar-icon"/>
+        icon: <img src={Dashboard} alt="Dashboard Icon" className="nav-bar-icon"/>
     },
     {
         title: 'Vendors',
@@ -24,5 +25,10 @@ export const NavigationPanelData = [
         title: 'Configurations',
         path: '/configurations',
         icon: <img src={Configurations} alt="Configuration Icon" className="nav-bar-icon"/>
+    },
+    {
+        title: 'Locations',
+        path: '/location',
+        icon: <img src={Locations} alt="Locations Icon" className="nav-bar-icon"/>
     },
 ]
