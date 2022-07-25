cd07f0dbf4892a24e0f3b23e4c4ed0a83b2b150c
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-25 22:11:01 +0530

    Updated Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,10 @@
-import React, {useEffect} from "react";
-
+import React from "react";
+import Dashboard from "../../Assets/dashboard.jpg";
 function Home() {
-
-    useEffect(() => {
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-    }, []);
 
     return (
         <div className="home">
-            <h1>Home</h1>
-
+            <img src={Dashboard} alt="Dashboard"/>
         </div>
     )
 }
