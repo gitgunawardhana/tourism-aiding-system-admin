7a79dc934ccda7b3e46ca2127cbe1378bd4bde55
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-25 22:15:16 +0530

    Updated Users.js

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -1,13 +1,9 @@
-import React, {useEffect} from "react";
+import React from "react";
 import UserTable from "../../Components/Users/UserTable/UserTable";
 import "./Users.css"
 import TextField from "@mui/material/TextField";
 
 function Users() {
-
-    useEffect(() => {
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-    }, []);
 
     return (
         <div className='main-section'>
