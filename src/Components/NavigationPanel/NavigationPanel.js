9fe66bdb9f3f5ed64bde0b7e60af0744453504d5
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-25 22:12:14 +0530

    Updated NavigationPanel.js

--- src/Components/NavigationPanel/NavigationPanel.js
+++ src/Components/NavigationPanel/NavigationPanel.js
@@ -4,50 +4,10 @@ import {NavigationPanelData} from "./NaigationPanelData";
 import './NavigationPanel.css';
 
 function NavigationPanel() {
-    // const navButtonClick = (title) => {
-    //     switch (title) {
-    //         case "Home":
-    //             setHome(true);
-    //             setVendor(false);
-    //             setUser(false);
-    //             setConfiguration(false);
-    //             break;
-    //         case "Vendors":
-    //             setHome(false);
-    //             setVendor(true);
-    //             setUser(false);
-    //             setConfiguration(false);
-    //             break;
-    //         default:
-    //             setCommon(true);
-    //     }
-    // }
-    // const [home, setHome] = useState(false);
-    // const [vendor, setVendor] = useState(false);
-    // const [user, setUser] = useState(false);
-    // const [configuration, setConfiguration] = useState(false);
-    // const [common, setCommon] = useState(false);
-
     return (
         <>
             <div className="lower-nav-bar">
                 {NavigationPanelData.map((item, index) => {
-                    // switch (item.title) {
-                    //     case "Home":
-                    //         setCommon(home);
-                    //         break;
-                    //     case "Users":
-                    //         setCommon(user);
-                    //         break;
-                    //     case "Vendors":
-                    //         setCommon(vendor);
-                    //         break;
-                    //     case "Configurations":
-                    //         setCommon(configuration);
-                    //         break;
-                    //     default:
-                    //         setCommon(true);
-                    // }
                     return (
                         <div key={index} className="nav-bar-item">
                             <Link to={item.path}>
@@ -60,7 +20,7 @@ function NavigationPanel() {
                 })}
             </div>
             <div className="upper-nav-bar">
-                <h1>ROADSIGN</h1>
+                <h1>ROADSIGN ADMIN PANEL</h1>
             </div>
         </>
     )
