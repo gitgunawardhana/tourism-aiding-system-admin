36dba9007add718d070b9b7e3f4989ce0749a92c
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-07-25 22:15:37 +0530

    Updated Vendors.js

--- src/Pages/Vendors/Vendors.js
+++ src/Pages/Vendors/Vendors.js
@@ -1,21 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import TextField from "@mui/material/TextField";
 import VendorTable from "../../Components/Vendors/VendorTable/VendorTable";
 import "./Vendors.css"
-import VendorForm from "../../Components/Vendors/VendorForm/VendorForm";
-import Button from "@mui/material/Button";
 
 function Vendors() {
-
-    useEffect(() => {
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-    }, []);
-
-
-    const [isFormOpen, setIsFormOpen] = useState(false);
-    const toggleFormPopup = () => {
-        setIsFormOpen(!isFormOpen);
-    }
     return (
         <div className='main-section'>
             <div className="title-box">
@@ -32,25 +20,12 @@ function Vendors() {
                             variant="outlined"
                         />
                     </div>
-                    <div className="column">
-                        <div className="vendor-new-button">
-                            <Button
-                                variant="contained"
-                                size="large"
-                                onClick={toggleFormPopup}>
-                                Add New Vendor
-                            </Button>
-                        </div>
-                    </div>
                 </div>
 
                 <div className="vendor-table">
                     <VendorTable/>
                 </div>
             </div>
-            {isFormOpen && <VendorForm
-                handleClose={toggleFormPopup}
-            />}
         </div>
     )
 }
