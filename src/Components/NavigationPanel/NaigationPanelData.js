448b6c5c2cc0da62abf1cfbaeb725d547e78920a
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-08-05 02:48:56 +0530

    Updated navigation panel data and functions

--- src/Components/NavigationPanel/NaigationPanelData.js
+++ src/Components/NavigationPanel/NaigationPanelData.js
@@ -9,26 +9,39 @@ export const NavigationPanelData = [
     {
         title: 'Dashboard',
         path: '/',
-        icon: <img src={Dashboard} alt="Dashboard Icon" className="nav-bar-icon"/>
+        icon: <img src={Dashboard} alt="Dashboard Icon" className="nav-bar-icon"/>,
+        subItems: []
     },
     {
         title: 'Locations',
         path: '/locations',
-        icon: <img src={Locations} alt="Locations Icon" className="nav-bar-icon"/>
+        icon: <img src={Locations} alt="Locations Icon" className="nav-bar-icon"/>,
+        subItems: []
     },
     {
         title: 'Vendors',
         path: '/vendors',
-        icon: <img src={Vendors} alt="Vendors Icon" className="nav-bar-icon"/>
+        icon: <img src={Vendors} alt="Vendors Icon" className="nav-bar-icon"/>,
+        subItems: []
     },
     {
         title: 'Users',
         path: '/users',
-        icon: <img src={Users} alt="Users Icon" className="nav-bar-icon"/>
+        icon: <img src={Users} alt="Users Icon" className="nav-bar-icon"/>,
+        subItems: []
     },
     {
         title: 'Configurations',
         path: '/configurations',
-        icon: <img src={Configurations} alt="Configuration Icon" className="nav-bar-icon"/>
+        icon: <img src={Configurations} alt="Configuration Icon" className="nav-bar-icon"/>,
+        subItems: [
+            {title: 'Driver Fare',path: ''},
+            {title: 'Vehicle Types',path: '/configurations/vehicle-types'},
+            {title: 'Accommodation Types',path: ''},
+            {title: 'Room Facilities',path: ''},
+            {title: 'Bathroom Facilities',path: ''},
+            {title: 'Provinces',path: ''},
+            {title: 'Location activities',path: ''},
+        ]
     },
 ]
--- src/Components/NavigationPanel/NavigationPanel.js
+++ src/Components/NavigationPanel/NavigationPanel.js
@@ -2,8 +2,13 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {NavigationPanelData} from "./NaigationPanelData";
 import './NavigationPanel.css';
+import Grid from "@mui/material/Grid";
+import DropDownIcon from "../../Assets/NavigationPanel/down-arrow.png";
 
 function NavigationPanel() {
+
+    const configurations = ["Driver Fare", "Vehicle Types", "Accommodation Types",
+        "Room Facilities", "Bathroom Facilities", "Provinces", "Location activities"];
     return (
         <>
             <div className="upper-nav-bar">
@@ -11,15 +16,104 @@ function NavigationPanel() {
             </div>
             <div className="side-bar">
                 <div className="logo-name">ROADSIGN</div>
-                {NavigationPanelData.map((item, index) => {
-                    return (
-                        <div key={index} className="nav-bar-item">
-                            <Link to={item.path}>
-                                <span>{item.title}</span>
-                            </Link>
-                        </div>
-                    )
-                })}
+
+                <div className="navigation-item-list">
+
+                    {NavigationPanelData.map((item) => (
+                        <>
+                            <Grid container spacing={0} className="navigation-item">
+                                <Grid item xs={2}>
+                                    {item.icon}
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Link to={item.path} className="navigation-item-link">
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <div
+                                        className={item.subItems.length !== 0 ? "navigation-sub-item-dropdown" : "navigation-sub-item-dropdown-hidden"}>
+                                        <img src={DropDownIcon} alt="Dropdown"/>
+                                    </div>
+                                </Grid>
+                            </Grid>
+                            {item.subItems.map((subItem) => (
+                                <Grid container spacing={0} className="navigation-item">
+                                    <Grid item xs={2}/>
+                                    <Grid item xs={9}>
+                                        <Link to={subItem.path} className="navigation-item-link">
+                                            <span>{subItem.title}</span>
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            ))}
+                        </>
+                    ))}
+
+                    {/*Configuration Sub List*/}
+                    {/*{configurations.map((item) => (*/}
+                    {/*    <Grid container spacing={0} className="navigation-item">*/}
+                    {/*        <Grid item xs={2}/>*/}
+                    {/*        <Grid item xs={9}>*/}
+                    {/*            <Link to="/configurations" className="navigation-item-link">*/}
+                    {/*                <span>{item}</span>*/}
+                    {/*            </Link>*/}
+                    {/*        </Grid>*/}
+                    {/*    </Grid>*/}
+                    {/*))}*/}
+                </div>
+
+                {/*<div className="nav-bar-item">*/}
+                {/*    <Link to="/configurations">*/}
+                {/*        <span>Configurations</span>*/}
+                {/*    </Link>*/}
+                {/*    <div className="nav-bar-sub-item-list">*/}
+                {/*        <div className="nav-bar-sub-item">*/}
+                {/*            <Link to="/configurations">*/}
+                {/*                <span>Driver fare</span>*/}
+                {/*            </Link>*/}
+                {/*        </div>*/}
+                {/*        <div className="nav-bar-sub-item">*/}
+                {/*            <Link to="/configurations">*/}
+                {/*                <span>Vehicle Types</span>*/}
+                {/*            </Link>*/}
+                {/*        </div>*/}
+
+                {/*        <div className="nav-bar-sub-item">*/}
+                {/*            <Link to="/configurations">*/}
+                {/*                <span>Accommodation Types</span>*/}
+                {/*            </Link>*/}
+                {/*        </div>*/}
+                {/*        <div className="nav-bar-sub-item">*/}
+                {/*            <Link to="/configurations">*/}
+                {/*                <span>Room Facilities</span>*/}
+                {/*            </Link>*/}
+                {/*        </div>*/}
+                {/*        <div className="nav-bar-sub-item">*/}
+                {/*            <Link to="/configurations">*/}
+                {/*                <span>Bathroom Facilities</span>*/}
+                {/*            </Link>*/}
+                {/*        </div>*/}
+                {/*        <div className="nav-bar-sub-item">*/}
+                {/*            <Link to="/configurations">*/}
+                {/*                <span>Accommodation Types</span>*/}
+                {/*            </Link>*/}
+                {/*        </div>*/}
+
+                {/*        <div className="nav-bar-sub-item">*/}
+                {/*            <Link to="/configurations">*/}
+                {/*                <span>Provinces</span>*/}
+                {/*            </Link>*/}
+                {/*        </div>*/}
+                {/*        <div className="nav-bar-sub-item">*/}
+                {/*            <Link to="/configurations">*/}
+                {/*                <span>Location Activities</span>*/}
+                {/*            </Link>*/}
+                {/*        </div>*/}
+
+                {/*    </div>*/}
+                {/*</div>*/}
+
             </div>
         </>
     )
