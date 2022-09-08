0fded98cff5096168b3c0db6b6b9a92dca745ba2
Diluni Malsha Peiris <diluni.malsha.peiris@gmail.com>
2022-09-08 12:01:36 +0530

    Updated codes

--- src/Pages/LocationAttraction/NewLocationAttraction.js
+++ src/Pages/LocationAttraction/NewLocationAttraction.js
@@ -6,13 +6,18 @@ import {useParams} from "react-router";
 import TextField from "@mui/material/TextField";
 import axios from "axios";
 
+const token = JSON.parse(sessionStorage.getItem('token'));
+const config = {
+    headers: {Authorization: `Bearer ` + token}
+};
+
 function NewLocationAttraction() {
     let locationId = useParams().locationId;
 
-    const [locationName, setLocationName] = useState("");
+    const [locationName, setLocationName] = useState(null);
 
     useEffect(() => {
-        axios.get("http://localhost:8080/admin/location/name/" + locationId)
+        axios.get("http://localhost:8080/admin/location/name/" + locationId, config)
             .then(res => {
                 setLocationName(res.data.body)
             })
@@ -24,20 +29,22 @@ function NewLocationAttraction() {
                 <p className="title">Add New Location Attraction</p>
                 <p className="sub-title">Locations / Attractions / Create Attraction</p>
             </div>
-            <div className="main-section">
-                <div className="card">
-                    <TextField
-                        id="locationName"
-                        label="Location Name"
-                        type="text"
-                        value={locationName}
-                        InputProps={{
-                            readOnly: true,
-                        }}
-                        sx={{m: 1, width: '97%'}}/>
+            {locationName != null ?
+                <div className="main-section">
+                    <div className="card">
+                        <TextField
+                            id="locationName"
+                            label="Location Name"
+                            type="text"
+                            value={locationName}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            sx={{m: 1, width: '97%'}}/>
+                    </div>
+                    <NewLocationAttractionForm locationId={locationId}/>
                 </div>
-                <NewLocationAttractionForm locationId={locationId}/>
-            </div>
+                : null}
         </>
     );
 }
